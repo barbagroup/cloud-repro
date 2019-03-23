11007edbca00290fc661266a527986645af759c3
mesnardo <mesnardo@gwu.edu>
2019-03-23 18:06:28 -0400

    Add script to plot isosurfaces of Q-criterion with VisIt
    * Save figure as PNG.

---
+++ examples/snake3d2k35/scripts/plot_qcrit_wx_wake3d_visit.py
@@ -0,0 +1,30 @@
+"""Plot the isosurfaces of the Q-criterion at save time steps.
+
+The isosurfaces are colored with the streamwise vorticity.
+"""
+
+import numpy
+import os
+import pathlib
+import sys
+
+scriptdir = pathlib.Path(__file__).absolute().parents[3] / 'misc'
+if scriptdir not in sys.path:
+    sys.path.insert(0, str(scriptdir))
+from visitplot import *
+
+
+simudir = pathlib.Path(__file__).absolute().parents[1]
+xdmfdir = simudir / 'output'
+config_view = simudir / 'scripts' / 'visit_view3d.yaml'
+figdir = simudir / 'figures'
+prefix = 'qcrit_wx_wake3d_'
+
+visit_plot_qcrit_wx_3d(xdmfdir,
+                       wx_range=(-5.0, 5.0),
+                       q_value=1.0,
+                       config_view=config_view,
+                       out_dir=figdir, out_prefix=prefix,
+                       figsize=(880, 372),
+                       visit_dir=os.environ.get('VISIT_DIR'),
+                       states=[1])
--- misc/visitplot.py
+++ misc/visitplot.py
@@ -141,7 +141,7 @@ def visit_plot_pseudocolor_2d(xdmf_path, name,
         visit.SaveWindow()
 
     os.remove('visitlog.py')
-    sys.exit(0)
+    visit.Close()
     return
 
 
@@ -274,5 +274,183 @@ def visit_plot_contour_3d(xdmf_path, name,
         visit.SaveWindow()
 
     os.remove('visitlog.py')
-    sys.exit(0)
+    visit.Close()
+    return
+
+
+def visit_plot_qcrit_wx_3d(xdmf_dir,
+                           wx_range=(-5.0, 5.0),
+                           q_value=0.1,
+                           config_view=None,
+                           out_dir=os.getcwd(), out_prefix='wake3d_',
+                           figsize=(1024, 1024),
+                           visit_dir=None, visit_arch='linux-x86_64',
+                           state=None, states=None,
+                           states_range=[0, None, 1]):
+    # Import VisIt package.
+    if visit_dir is None:
+        visit_dir = os.environ.get('VISIT_DIR')
+        if visit_dir is None:
+            raise ValueError('Provide VisIt installation path or '
+                             'set env variable VISIT_DIR')
+    sys.path.append(os.path.join(visit_dir, visit_arch,
+                    'lib', 'site-packages'))
+    import visit
+
+    visit.LaunchNowin()
+
+    # Check version of VisIt.
+    visit_check_version(visit.Version())
+
+    # Define some variables to get the q_crit and wx_cc.
+    p_xdmf_path = os.path.join(str(xdmf_dir), 'p.xmf')
+    visit.OpenDatabase(p_xdmf_path, 0)
+    visit.DefineScalarExpression("operators/ConnectedComponents/p Grid", "cell_constant(<p Grid>, 0.)")
+    visit.DefineCurveExpression("operators/DataBinning/1D/p Grid", "cell_constant(<p Grid>, 0)")
+    visit.DefineScalarExpression("operators/DataBinning/2D/p Grid", "cell_constant(<p Grid>, 0)")
+    visit.DefineScalarExpression("operators/DataBinning/3D/p Grid", "cell_constant(<p Grid>, 0)")
+    visit.DefineScalarExpression("operators/Flux/p Grid", "cell_constant(<p Grid>, 0.)")
+    visit.DefineCurveExpression("operators/Lineout/p", "cell_constant(<p>, 0.)")
+    visit.DefineCurveExpression("operators/Lineout/time_derivative/p Grid_time", "cell_constant(time_derivative/p Grid_time, 0.)")
+    visit.DefineCurveExpression("operators/Lineout/time_derivative/p Grid_lasttime", "cell_constant(time_derivative/p Grid_lasttime, 0.)")
+    visit.DefineCurveExpression("operators/Lineout/time_derivative/p", "cell_constant(time_derivative/p, 0.)")
+    visit.DefineScalarExpression("operators/ModelFit/model", "point_constant(<p Grid>, 0)")
+    visit.DefineScalarExpression("operators/ModelFit/distance", "point_constant(<p Grid>, 0)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Sum/p", "cell_constant(<p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Mean/p", "cell_constant(<p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Variance/p", "cell_constant(<p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Std. Dev./p", "cell_constant(<p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Slope/p", "cell_constant(<p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Residuals/p", "cell_constant(<p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Sum/time_derivative/p Grid_time", "cell_constant(<time_derivative/p Grid_time>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Sum/time_derivative/p Grid_lasttime", "cell_constant(<time_derivative/p Grid_lasttime>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Sum/time_derivative/p", "cell_constant(<time_derivative/p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Mean/time_derivative/p Grid_time", "cell_constant(<time_derivative/p Grid_time>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Mean/time_derivative/p Grid_lasttime", "cell_constant(<time_derivative/p Grid_lasttime>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Mean/time_derivative/p", "cell_constant(<time_derivative/p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Variance/time_derivative/p Grid_time", "cell_constant(<time_derivative/p Grid_time>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Variance/time_derivative/p Grid_lasttime", "cell_constant(<time_derivative/p Grid_lasttime>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Variance/time_derivative/p", "cell_constant(<time_derivative/p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Std. Dev./time_derivative/p Grid_time", "cell_constant(<time_derivative/p Grid_time>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Std. Dev./time_derivative/p Grid_lasttime", "cell_constant(<time_derivative/p Grid_lasttime>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Std. Dev./time_derivative/p", "cell_constant(<time_derivative/p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Slope/time_derivative/p Grid_time", "cell_constant(<time_derivative/p Grid_time>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Slope/time_derivative/p Grid_lasttime", "cell_constant(<time_derivative/p Grid_lasttime>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Slope/time_derivative/p", "cell_constant(<time_derivative/p>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Residuals/time_derivative/p Grid_time", "cell_constant(<time_derivative/p Grid_time>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Residuals/time_derivative/p Grid_lasttime", "cell_constant(<time_derivative/p Grid_lasttime>, 0.)")
+    visit.DefineScalarExpression("operators/StatisticalTrends/Residuals/time_derivative/p", "cell_constant(<time_derivative/p>, 0.)")
+    visit.DefineVectorExpression("operators/SurfaceNormal/p Grid", "cell_constant(<p Grid>, 0.)")
+    # Define cell-centered velocity vector field.
+    ux_xdmf_path = os.path.join(str(xdmf_dir), 'u.xmf')
+    uy_xdmf_path = os.path.join(str(xdmf_dir), 'v.xmf')
+    uz_xdmf_path = os.path.join(str(xdmf_dir), 'w.xmf')
+    vel_exp = ('{' +
+               'pos_cmfe(<{}[0]id:u>, <p Grid>, 1.0),'.format(ux_xdmf_path) +
+               'pos_cmfe(<{}[0]id:v>, <p Grid>, 0.0),'.format(uy_xdmf_path) +
+               'pos_cmfe(<{}[0]id:w>, <p Grid>, 0.0)'.format(uz_xdmf_path) +
+               '}')
+    visit.DefineVectorExpression('velocity', vel_exp)
+    # Define Q-criterion.
+    qcrit_exp = ('q_criterion(' +
+                 'gradient(velocity[0]),' +
+                 'gradient(velocity[1]),' +
+                 'gradient(velocity[2])' +
+                 ')')
+    visit.DefineScalarExpression('q_crit', qcrit_exp)
+    # Define cell-centered streamwise vorticity.
+    wx_xdmf_path = os.path.join(str(xdmf_dir), 'wx.xmf')
+    wx_exp = 'pos_cmfe(<{}[0]id:wx>, <p Grid>, 0.0)'.format(wx_xdmf_path)
+    visit.DefineScalarExpression('wx_cc', wx_exp)
+
+    # Add a pseudocolor of the cell-centered streamwise vorticity.
+    visit.AddPlot('Pseudocolor', 'wx_cc', 1, 1)
+    PseudocolorAtts = visit.PseudocolorAttributes()
+    PseudocolorAtts.minFlag = 1
+    PseudocolorAtts.min = wx_range[0]
+    PseudocolorAtts.maxFlag = 1
+    PseudocolorAtts.max = wx_range[1]
+    PseudocolorAtts.colorTableName = 'viridis'
+    PseudocolorAtts.invertColorTable = 1
+    PseudocolorAtts.opacityType = PseudocolorAtts.Constant
+    PseudocolorAtts.opacity = 0.8
+    PseudocolorAtts.legendFlag = 0
+    visit.SetPlotOptions(PseudocolorAtts)
+
+    # Add an isosurface of the Q-criterion.
+    visit.AddOperator('Isosurface', 1)
+    IsosurfaceAtts = visit.IsosurfaceAttributes()
+    IsosurfaceAtts.variable = 'q_crit'
+    IsosurfaceAtts.contourMethod = IsosurfaceAtts.Value
+    IsosurfaceAtts.contourValue = (q_value)
+    IsosurfaceAtts.scaling = IsosurfaceAtts.Linear
+    visit.SetOperatorOptions(IsosurfaceAtts, 1)
+
+    # Remove info about user, time, database, and legend.
+    AnnotationAtts = visit.AnnotationAttributes()
+    AnnotationAtts.userInfoFlag = 0
+    AnnotationAtts.databaseInfoFlag = 0
+    AnnotationAtts.timeInfoFlag = 0
+    AnnotationAtts.legendInfoFlag = 0
+    AnnotationAtts.axes3D.visible = 0
+    AnnotationAtts.axes3D.triadFlag = 1
+    AnnotationAtts.axes3D.bboxFlag = 0
+    visit.SetAnnotationAttributes(AnnotationAtts)
+
+    # Parse the 3D view configuration file.
+    if config_view is not None:
+        with open(str(config_view), 'r') as infile:
+            config_view = yaml.load(infile, Loader=yaml.FullLoader)['View3DAtts']
+    # Set attributes of the view.
+    View3DAtts = visit.View3DAttributes()
+    for key, value in config_view.items():
+        if type(value) is list:
+            value = tuple(value)
+        setattr(View3DAtts, key, value)
+    visit.SetView3D(View3DAtts)
+
+    visit.DrawPlots()
+    visit.SetActiveWindow(1)
+
+    visit.Source(os.path.join(visit_dir, visit_arch, 'bin', 'makemovie.py'))
+    visit.ToggleCameraViewMode()
+
+    # Create output directory if necessary.
+    if not os.path.isdir(str(out_dir)):
+        os.makedirs(str(out_dir))
+
+    # Loop over the states to render and save the plots.
+    if state is not None:
+        states = [state]
+    elif states is None:
+        if states_range[1] is None:
+            states_range[1] = visit.TimeSliderGetNStates()
+        else:
+            states_range[1] += 1
+        states = range(*states_range)
+
+    for state in states:
+        print('[state {}] Rendering and saving figure ...'.format(state))
+        visit.SetTimeSliderState(state)
+
+        RenderingAtts = visit.RenderingAttributes()
+        visit.SetRenderingAttributes(RenderingAtts)
+
+        SaveWindowAtts = visit.SaveWindowAttributes()
+        SaveWindowAtts.outputToCurrentDirectory = 0
+        SaveWindowAtts.outputDirectory = str(out_dir)
+        SaveWindowAtts.fileName = '{}{:0>4}'.format(out_prefix, state)
+        SaveWindowAtts.family = 0
+        SaveWindowAtts.format = SaveWindowAtts.PNG
+        SaveWindowAtts.width = figsize[0]
+        SaveWindowAtts.height = figsize[1]
+        SaveWindowAtts.quality = 100
+        SaveWindowAtts.resConstraint = SaveWindowAtts.NoConstraint
+        visit.SetSaveWindowAttributes(SaveWindowAtts)
+
+        visit.SaveWindow()
+
+    os.remove('visitlog.py')
+    visit.CloseComputeEngine()
+    visit.Close()
     return
